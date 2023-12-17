03af8cbea374d94dcd3aa66afde95e2ceb340013
José Gabriel González <jgg032899@gmail.com>
2023-12-16 21:29:07 -0400

    Horizontal camera movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform viewPoint;
+    public float mouseSensivity = 1f;
+    private float verticalRotStore;
+    private Vector2 mouseInput;
+
+    private void Update()
+    {
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensivity;
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z); ;
+    }
+}
