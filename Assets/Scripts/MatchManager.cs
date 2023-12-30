e3bea2cb84c46206dccd860ff704618423d6faf7
José Gabriel González <jgg032899@gmail.com>
2023-12-30 00:06:49 -0400

    Update MatchManager.cs

--- Assets/Scripts/MatchManager.cs
+++ Assets/Scripts/MatchManager.cs
@@ -1,10 +1,23 @@
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.SceneManagement;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+using System.Collections.Generic;
 
-public class MatchManager : MonoBehaviour
+public class MatchManager : MonoBehaviourPunCallbacks, IOnEventCallback
 {
     public static MatchManager Instance;
+
+    public enum EventCodes : byte
+    {
+        NewPlayer,
+        ListPlayers,
+        UpdateStat
+    }
+
+    public List<PlayerInfo> allPlayers = new List<PlayerInfo>();
+    private int index;
 
     private void Awake()
     {
@@ -17,5 +30,103 @@ public class MatchManager : MonoBehaviour
         {
             SceneManager.LoadScene(0);
         }
+        else
+        {
+            NewPlayerSend(PhotonNetwork.NickName);
+        }
+    }
+
+    public void OnEvent(EventData photonEvent)
+    {
+        if (photonEvent.Code < 200)
+        {
+            EventCodes theEvent = (EventCodes)photonEvent.Code;
+            object[] data = (object[])photonEvent.CustomData;
+
+            //Debug.Log("Received event: " + theEvent);
+
+            switch (theEvent)
+            {
+                case EventCodes.NewPlayer:
+                    NewPlayerReceive(data);
+                    break;
+
+                case EventCodes.ListPlayers:
+                    ListPlayersReceive(data);
+                    break;
+
+                case EventCodes.UpdateStat:
+                    UpdateStatsReceive(data);
+                    break;
+            }
+        }
+    }
+
+    public override void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    public override void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    public void NewPlayerSend(string username)
+    {
+        object[] package = new object[4];
+        package[0] = username;
+        package[1] = PhotonNetwork.LocalPlayer.ActorNumber;
+        package[2] = 0;
+        package[3] = 0;
+
+        PhotonNetwork.RaiseEvent(
+            (byte)EventCodes.NewPlayer,
+            package,
+            new RaiseEventOptions { Receivers = ReceiverGroup.MasterClient },
+            new SendOptions { Reliability = true }
+        );
+    }
+
+    public void NewPlayerReceive(object[] dataReceived)
+    {
+        PlayerInfo player = new PlayerInfo((string)dataReceived[0], (int)dataReceived[1], (int)dataReceived[2], (int)dataReceived[3]);
+
+        allPlayers.Add(player);
+    }
+
+    public void ListPlayersSend()
+    {
+
+    }
+
+    public void ListPlayersReceive(object[] dataReceived)
+    {
+
+    }
+
+    public void UpdateStatsSend()
+    {
+
+    }
+
+    public void UpdateStatsReceive(object[] dataReceived)
+    {
+
+    }
+}
+
+[System.Serializable]
+public class PlayerInfo
+{
+    public string name;
+    public int actor, kills, deaths;
+
+    public PlayerInfo(string _name, int _actor, int _kills, int _deaths)
+    {
+        name = _name;
+        actor = _actor;
+        kills = _kills;
+        deaths = _deaths;
     }
 }
